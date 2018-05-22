28cedd634b37ac35b63e4d04befa5c1f230f8d8d
Davi Cesar Correia Jr <davi@qulture.rocks>
2018-05-22 20:31:26 -0300

    Change init.c to call hello_world() sys_call

--- 1/initramfs/init.c
+++ 1/initramfs/init.c
@@ -1,9 +1,19 @@
-#include <stdio.h>
+// #include <stdio.h>
+
+// int main()
+// {
+//    while (1) { 
+//       printf("Hello, World; from user!\n");
+//    }
+//    return 0;
+// }
+
+#include "hello_world.h"
 
 int main()
 {
-   while (1) { 
-      printf("Hello, World; from user!\n");
+   while (1) {
+      hello_world();
    }
    return 0;
 }
